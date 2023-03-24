cba11860aa34a13503d82c0bbf489754aa794aab
yra-stasyshyn <stasyshynyurii.mm@gmail.com>
2023-03-24 17:47:20 -0300

    minor change to try to solve cell phone issue

--- src/components/Sections/HeroSection/styles.tsx
+++ src/components/Sections/HeroSection/styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
-	height: 100vh;
+	height: 100%;
 	width: 100%;
 	overflow: hidden;
 	position: fixed;
--- src/components/SlideShow/styles.tsx
+++ src/components/SlideShow/styles.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const Wrapper = styled.div`
 	position: relative;
-	height: 100vh;
+	height: 100%;
 	overflow: hidden;
 	display: flex;
 	align-items: flex-end;
