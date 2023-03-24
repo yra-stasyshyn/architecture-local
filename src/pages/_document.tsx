c3eefd8f4f8ff426ca9256e3563e0893c042be65
yra-stasyshyn <stasyshynyurii.mm@gmail.com>
2023-03-24 17:28:19 -0300

    adds viewport tag on the head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -54,6 +54,7 @@ export default class MyDocument extends Document {
 					href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700&display=swap"
 					rel="stylesheet"
 				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<meta name="theme-color" content="#FFFFFF" />
 				<body>
 					<Main />
