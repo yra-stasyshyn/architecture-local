e6027dad4f5c242722e0f81dd1b8aef36b858b69
Pedro Mesquita <pedrodardengo@hotmail.com>
2023-03-25 14:55:08 -0300

    adds a loading screen with progress bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import HeroSection from '@/components/Sections/HeroSection'
 import NavBar from '@/components/NavBar'
 import React from 'react'
 import * as S from '../components/Sections/styles'
+import LoadingScreen from '@/components/LoadingScreen'
 
 export default function Home() {
 	return (
 		<>
+			<LoadingScreen />
 			<HeroSection />
 			<NavBar />
 			<S.SectionsContentWrapper></S.SectionsContentWrapper>
