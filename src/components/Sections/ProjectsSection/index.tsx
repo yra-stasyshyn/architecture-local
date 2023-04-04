a1f66fe05a0b57e2e031ebc4724fab7b8c7b0e8d
yra-stasyshyn <stasyshynyurii.mm@gmail.com>
2023-04-04 13:15:37 -0300

    changes projects section to use next images

--- src/components/Sections/ProjectsSection/index.tsx
+++ src/components/Sections/ProjectsSection/index.tsx
@@ -3,26 +3,37 @@ import * as S from './styles'
 import ProjectsGrid from '@/components/ProjectsGrid'
 import NextImage from 'next/image'
 
+import img06 from '../../../../public/img/06.png'
+import img07 from '../../../../public/img/07.png'
+import img01 from '../../../../public/img/01.png'
+import img02 from '../../../../public/img/02.jpg'
+import img04 from '../../../../public/img/04.png'
+import img05 from '../../../../public/img/05.png'
+import img09 from '../../../../public/img/09.png'
+import img10 from '../../../../public/img/10.png'
+import img11 from '../../../../public/img/11.png'
+
 const ProjectsSection = React.forwardRef<HTMLDivElement>((props, ref) => {
+	const quality = 30
 	return (
 		<S.Wrapper ref={ref}>
 			<ProjectsGrid>
-				<NextImage src={'/img/12.jpg'} alt={'casa local 1'} fill={true} />
-				<NextImage src={'/img/08.jpg'} alt={'casa local 2'} fill={true} />
-				<NextImage src={'/img/06.png'} alt={'casa local 3'} fill={true} />
-				<NextImage src={'/img/07.png'} alt={'casa local 4'} fill={true} />
-				<NextImage src={'/img/01.png'} alt={'casa local 5'} fill={true} />
-				<NextImage src={'/img/02.jpg'} alt={'casa local 6'} fill={true} />
-				<NextImage src={'/img/04.png'} alt={'casa local 7'} fill={true} />
-				<NextImage src={'/img/12.jpg'} alt={'casa local 1'} fill={true} />
-				<NextImage src={'/img/08.jpg'} alt={'casa local 2'} fill={true} />
-				<NextImage src={'/img/06.png'} alt={'casa local 3'} fill={true} />
-				<NextImage src={'/img/07.png'} alt={'casa local 4'} fill={true} />
-				<NextImage src={'/img/01.png'} alt={'casa local 5'} fill={true} />
-				<NextImage src={'/img/02.jpg'} alt={'casa local 6'} fill={true} />
-				<NextImage src={'/img/04.png'} alt={'casa local 7'} fill={true} />
-				<NextImage src={'/img/12.jpg'} alt={'casa local 1'} fill={true} />
-				<NextImage src={'/img/08.jpg'} alt={'casa local 2'} fill={true} />
+				<NextImage src={img06} alt={'casa local 1'} quality={quality} />
+				<NextImage src={img07} alt={'casa local 2'} quality={quality} />
+				<NextImage src={img01} alt={'casa local 3'} quality={quality} />
+				<NextImage src={img02} alt={'casa local 4'} quality={quality} />
+				<NextImage src={img04} alt={'casa local 5'} quality={quality} />
+				<NextImage src={img05} alt={'casa local 6'} quality={quality} />
+				<NextImage src={img09} alt={'casa local 7'} quality={quality} />
+				<NextImage src={img10} alt={'casa local 1'} quality={quality} />
+				<NextImage src={img11} alt={'casa local 2'} quality={quality} />
+				<NextImage src={img06} alt={'casa local 3'} quality={quality} />
+				<NextImage src={img07} alt={'casa local 4'} quality={quality} />
+				<NextImage src={img01} alt={'casa local 5'} quality={quality} />
+				<NextImage src={img02} alt={'casa local 6'} quality={quality} />
+				<NextImage src={img04} alt={'casa local 7'} quality={quality} />
+				<NextImage src={img05} alt={'casa local 1'} quality={quality} />
+				<NextImage src={img09} alt={'casa local 2'} quality={quality} />
 			</ProjectsGrid>
 		</S.Wrapper>
 	)
