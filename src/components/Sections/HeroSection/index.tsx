fb212141c456c762e24a50f7e4ab70acef05c45f
Pedro Mesquita <pedrodardengo@hotmail.com>
2023-04-06 19:48:03 -0300

    trying to solve constantly requesting images

--- src/components/Sections/HeroSection/index.tsx
+++ src/components/Sections/HeroSection/index.tsx
@@ -1,25 +1,21 @@
-import SlideShow from '@/components/SlideShow'
 import React from 'react'
 import * as S from './styles'
 
 const HeroSection = () => {
-	return (
-		<S.Wrapper>
-			<SlideShow>
-				{/*<img src={'/img/12.jpg'} alt={''} />*/}
-				{/*<img src={'/img/08.jpg'} alt={''} />*/}
-				{/*<img src={'/img/06.png'} alt={''} />*/}
-				{/*<img src={'/img/07.png'} alt={''} />*/}
-				{/*<img src={'/img/01.png'} alt={''} />*/}
-				{/*<img src={'/img/02.jpg'} alt={''} />*/}
-				{/*<img src={'/img/04.png'} alt={''} />*/}
-				{/*<img src={'/img/05.png'} alt={''} />*/}
-				{/*<img src={'/img/09.png'} alt={''} />*/}
-				{/*<img src={'/img/10.png'} alt={''} />*/}
-				{/*<img src={'/img/11.png'} alt={''} />*/}
-			</SlideShow>
-		</S.Wrapper>
-	)
+	return <S.Wrapper></S.Wrapper>
 }
 
 export default HeroSection
+// <SlideShow>
+// {/*<img src={'/img/12.jpg'} alt={''} />*/}
+// {/*<img src={'/img/08.jpg'} alt={''} />*/}
+// {/*<img src={'/img/06.png'} alt={''} />*/}
+// {/*<img src={'/img/07.png'} alt={''} />*/}
+// {/*<img src={'/img/01.png'} alt={''} />*/}
+// {/*<img src={'/img/02.jpg'} alt={''} />*/}
+// {/*<img src={'/img/04.png'} alt={''} />*/}
+// {/*<img src={'/img/05.png'} alt={''} />*/}
+// {/*<img src={'/img/09.png'} alt={''} />*/}
+// {/*<img src={'/img/10.png'} alt={''} />*/}
+// {/*<img src={'/img/11.png'} alt={''} />*/}
+// </SlideShow>
