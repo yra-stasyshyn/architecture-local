f8dd329e9fb17f52cd6a521f1cc4b442a3d5f2e5
Pedro Mesquita <pedrodardengo@hotmail.com>
2023-04-07 00:04:25 -0300

    trying to solve constantly requesting images

--- src/components/SlideShow/index.tsx
+++ src/components/SlideShow/index.tsx
@@ -9,10 +9,7 @@ const SlideShow = ({
 }) => {
 	const numberOfImages = children.length
 	const [currentImageIndex, setCurrentImageIndex] = React.useState(0)
-	const [imageDimensions, setImageDimensions] = React.useState({
-		naturalWidth: 0,
-		naturalHeight: 0
-	})
+
 	const windowDimensions = useWindowDimensions()
 	function nextImageIndex() {
 		if (currentImageIndex >= numberOfImages - 1) {
@@ -23,15 +20,6 @@ const SlideShow = ({
 	}
 
 	React.useEffect(() => {
-		const img = new Image()
-		img.onload = () => {
-			setImageDimensions({
-				naturalWidth: img.naturalWidth,
-				naturalHeight: img.naturalHeight
-			})
-		}
-		img.src = children[currentImageIndex].props.src
-
 		const onInterval = setInterval(() => {
 			nextImageIndex()
 		}, 3000)
@@ -43,25 +31,22 @@ const SlideShow = ({
 
 	const windowProportion =
 		windowDimensions.windowWidth / windowDimensions.windowHeight
-	const imageProportion =
-		imageDimensions.naturalWidth / imageDimensions.naturalHeight
-	let style: React.CSSProperties = {}
-	if (windowProportion < imageProportion) {
-		style = { maxWidth: 'none', height: '100%' }
-	} else {
-		style = { maxHeight: 'none', width: '100%' }
-	}
+
 	return (
 		<S.Wrapper>
 			{React.Children.map(children, (child, index) => {
 				const opacity = index === currentImageIndex ? '1' : '0'
-				const imageStyle: React.CSSProperties = {
-					...style,
-					opacity
-				}
-				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-				// @ts-ignore
-				return React.cloneElement(child, { style: imageStyle })
+				const imageStyle: React.CSSProperties = { opacity }
+				const windowToImageRatio =
+					windowProportion / (child.props.width / child.props.height)
+				return (
+					<S.ImageWrapper windowToImageRatio={windowToImageRatio}>
+						{/*// eslint-disable-next-line @typescript-eslint/ban-ts-comment*/}
+						{/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+						{/*// @ts-ignore*/}
+						{React.cloneElement(child, { style: imageStyle })}
+					</S.ImageWrapper>
+				)
 			})}
 		</S.Wrapper>
 	)
--- src/components/SlideShow/styles.tsx
+++ src/components/SlideShow/styles.tsx
@@ -4,19 +4,27 @@ export const Wrapper = styled.div`
 	position: relative;
 	height: 100%;
 	overflow: hidden;
-	display: flex;
-	align-items: flex-end;
-	justify-content: center;
 	z-index: -10;
 
+	> div:first-child {
+		> img {
+			opacity: 1;
+		}
+	}
+`
+
+export const ImageWrapper = styled.div<{ windowToImageRatio: number }>`
+	display: flex;
+	justify-content: center;
 	img {
 		position: absolute;
 		opacity: 0;
 		transition: opacity 400ms ease;
+		bottom: 0;
 		z-index: -10;
-	}
-
-	img:first-child {
-		opacity: 1;
+		max-width: ${(props) => (props.windowToImageRatio < 1 ? 'none' : '100%')};
+		max-height: ${(props) => (props.windowToImageRatio < 1 ? '100%' : 'none')};
+		height: ${(props) => (props.windowToImageRatio < 1 ? '100%' : 'auto')};
+		width: ${(props) => (props.windowToImageRatio < 1 ? 'auto' : '100%')};
 	}
 `
