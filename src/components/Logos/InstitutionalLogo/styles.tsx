f067e1a9da48064a9e9416ab1c69e506fd98c658
Pedro Mesquita <pedrodardengo@hotmail.com>
2023-04-07 11:00:16 -0300

    trying to solve constantly requesting images

---
+++ src/components/Logos/InstitutionalLogo/styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const LogoWrapper = styled.div`
+	position: relative;
+	width: 50%;
+	display: flex;
+	align-items: center;
+
+	img {
+		position: absolute;
+	}
+`
