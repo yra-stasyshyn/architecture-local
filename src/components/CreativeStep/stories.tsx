bc355aa4d90ca472583da8de368b6219ee8afc1d
Pedro Mesquita <pedrodardengo@hotmail.com>
2023-04-09 21:17:44 -0300

    fix stories

--- src/components/CreativeStep/stories.tsx
+++ src/components/CreativeStep/stories.tsx
@@ -1,9 +1,19 @@
 import CreativeStep from '.'
 import { Meta, StoryFn } from '@storybook/react'
+import React from 'react'
 
 export default {
 	title: 'CreativeStep',
 	component: CreativeStep
 } as Meta
 
-export const Default: StoryFn = () => <CreativeStep />
+export const Default: StoryFn = () => (
+	<CreativeStep
+		stepNumber={2}
+		stepTitle={'contexto'}
+		stepImageSrc={'/img/al-process-02.svg'}
+	>
+		Em seguida, partimos para o que é o centro da prática do estúdio: o estudo e
+		a análise do local e do contexto em torno da demanda.
+	</CreativeStep>
+)
