058515b4178ae063e655f8135ba30d2e25c6c53d
yra-stasyshyn <stasyshynyurii.mm@gmail.com>
2023-04-12 22:34:50 -0300

    changes place of instagram feed section

---
+++ src/components/Sections/AboutUsSection/InstagramFeedSection/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import * as S from './styles'
+import InstagramFeed from '@/components/InstagramFeed'
+
+const InstagramFeedSection = () => {
+	return (
+		<S.Wrapper>
+			<InstagramFeed />
+		</S.Wrapper>
+	)
+}
+
+export default InstagramFeedSection
--- src/components/Sections/ProjectsSection/styles.tsx
+++ src/components/Sections/ProjectsSection/styles.tsx
@@ -9,19 +9,3 @@ export const Wrapper = styled.div`
 	gap: 150px;
 	background-color: ${THEME.colors.secondaryColor};
 `
-
-export const InstaFeedDiv = styled.div`
-	position: relative;
-	left: 12.5%;
-	width: 62.5%;
-
-	@media (max-width: ${THEME.screenSize.tablet}px) {
-		left: 16.6%;
-		width: 66.6%;
-	}
-
-	@media (max-width: ${THEME.screenSize.mobile}px) {
-		left: 12.5%;
-		width: 75%;
-	}
-`
