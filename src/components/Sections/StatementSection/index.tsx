46994ba97ba9921c44ce9d634f61249d70db3f04
yra-stasyshyn <stasyshynyurii.mm@gmail.com>
2023-04-20 18:38:41 -0300

    changes first statement line breaks for different screen sizes

--- src/components/Sections/StatementSection/index.tsx
+++ src/components/Sections/StatementSection/index.tsx
@@ -1,17 +1,39 @@
 import React from 'react'
 import * as S from './styles'
 import Statement from '@/components/Statement'
+import { useWindowDimensions } from '@/utils/useWindowDimensions'
+import THEME from '@/styles/theme'
 
 const StatementSection = () => {
+	const [statement, setStatement] = React.useState(<></>)
+	const windowDimension = useWindowDimensions()
+
+	React.useEffect(() => {
+		if (windowDimension.windowWidth < THEME.screenSize.mobile) {
+			setStatement(
+				<>
+					Criamos projetos <br />
+					autênticos para <br />
+					pessoas e empresas <br />
+					que buscam <br />
+					melhorar vidas e <br />
+					cidades.
+				</>
+			)
+		} else {
+			setStatement(
+				<>
+					Criamos projetos autênticos para <br />
+					pessoas e empresas que buscam <br />
+					melhorar vidas e cidades.
+				</>
+			)
+		}
+	}, [windowDimension])
+
 	return (
 		<S.Wrapper>
-			<Statement>
-				Criamos projetos autênticos para
-				<br />
-				pessoas e empresas que buscam
-				<br />
-				melhorar vidas e cidades.
-			</Statement>
+			<Statement>{statement}</Statement>
 		</S.Wrapper>
 	)
 }
